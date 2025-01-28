a7953adde4912ff658a897fc60df63965ebc94b6
Cédric Belin <cedric@belin.io>
2025-01-28 10:43:41 +0100

    Add options shared by all DB commands

---
+++ src/commands/mysql/dsn_option.cs
@@ -0,0 +1,14 @@
+namespace Belin.Cli.Commands;
+
+/// <summary>
+/// Provides the connection string of a data source.
+/// </summary>
+public class DsnOption: Option<string> {
+
+	/// <summary>
+	/// Creates a new option.
+	/// </summary>
+	public DsnOption(): base(["-d", "--dsn"], "The connection string.") {
+		IsRequired = true;
+	}
+}
