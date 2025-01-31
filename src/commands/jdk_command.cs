376c18ee54e45b54385c944b5ce63b8af949dc53
Cédric Belin <cedric@belin.io>
2025-01-31 22:41:24 +0100

    Drop support for Linux and macOS platforms

--- src/commands/jdk_command.cs
+++ src/commands/jdk_command.cs
@@ -29,9 +29,8 @@ public class JdkCommand: Command {
 
 		var startInfo = new ProcessStartInfo("bin/java", ["--version"]) { CreateNoWindow = true, RedirectStandardOutput = true, WorkingDirectory = output.FullName };
 		using var process = Process.Start(startInfo) ?? throw new Exception(@"The ""java --version"" process could not be started.");
-		var stdout = process.StandardOutput.ReadToEnd().Trim();
-		await process.WaitForExitAsync();
-		Console.WriteLine(stdout);
+		Console.WriteLine(process.StandardOutput.ReadToEnd().Trim());
+		process.WaitForExit();
 	}
 
 	/// <summary>
@@ -41,13 +40,7 @@ public class JdkCommand: Command {
 	/// <param name="java">The major version of the Java development kit.</param>
 	/// <returns>The path to the downloaded ZIP archive.</returns>
 	private static async Task<string> DownloadArchive(HttpClient httpClient, int java) {
-		var os = true switch {
-			true when OperatingSystem.IsMacOS() => "macOS",
-			true when OperatingSystem.IsWindows() => "windows",
-			_ => "linux"
-		};
-
-		var path = Path.Join(Path.GetTempPath(), $"microsoft-jdk-{java}-{os}-x64.{(OperatingSystem.IsWindows() ? "zip" : "tar.gz")}");
+		var path = Path.Join(Path.GetTempPath(), $"microsoft-jdk-{java}-windows-x64.zip");
 		var file = Path.GetFileName(path);
 
 		Console.WriteLine($"Downloading file \"{file}\"...");
@@ -63,15 +56,7 @@ public class JdkCommand: Command {
 	/// <param name="output">The path to the output directory.</param>
 	private static void ExtractArchive(string path, DirectoryInfo output) {
 		Console.WriteLine($"Extracting file \"{Path.GetFileName(path)}\" into directory \"{output.FullName}\"...");
-
-		if (string.Equals(Path.GetExtension(path), ".zip", StringComparison.InvariantCultureIgnoreCase)) {
-			using var zipArchive = ZipFile.OpenRead(path);
-			zipArchive.ExtractToDirectory(output.FullName, overwriteFiles: true);
-		}
-		else {
-			var args = new[] { $"--directory={output.FullName}", "--extract", $"--file={path}", "--strip-components=1" };
-			using var process = Process.Start("tar", args) ?? throw new Exception(@"The ""tar"" process could not be started.");
-			process.WaitForExit(); // TODO ExitCode != 0 throw Exception
-		}
+		using var zipArchive = ZipFile.OpenRead(path);
+		zipArchive.ExtractToDirectory(output.FullName, overwriteFiles: true);
 	}
 }
