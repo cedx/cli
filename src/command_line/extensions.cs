cc57aab61598af0002d0f274c517afb17fc20ecf
Cédric Belin <cedric@belin.io>
2025-02-02 01:05:55 +0100

    Code factorization: add the `Extensions` class

---
+++ src/command_line/extensions.cs
@@ -0,0 +1,59 @@
+namespace Belin.Cli.CommandLine;
+
+using System.Diagnostics;
+using System.IO.Compression;
+using System.Reflection;
+
+/// <summary>
+/// Provides extension method for the <see cref="Command"/> class.
+/// </summary>
+public static class Extensions {
+
+	/// <summary>
+	/// Creates a new HTTP client.
+	/// </summary>
+	/// <param name="_">The current command.</param>
+	/// <returns>The newly created HTTP client.</returns>
+	public static HttpClient CreateHttpClient(this Command _) {
+		var fileVersion = Assembly.GetExecutingAssembly().GetCustomAttribute<AssemblyFileVersionAttribute>();
+		var userAgent = $".NET/{Environment.Version} | Belin.io/{new Version(fileVersion!.Version).ToString(3)}";
+		var httpClient = new HttpClient();
+		httpClient.DefaultRequestHeaders.Add("user-agent", userAgent);
+		return httpClient;
+	}
+
+	/// <summary>
+	/// Extracts the specified ZIP file into a given directory.
+	/// </summary>
+	/// <param name="_">The current command.</param>
+	/// <param name="input">The path to the input ZIP file.</param>
+	/// <param name="output">The path to the output directory.</param>
+	public static void ExtractZipFile(this Command _, FileInfo input, DirectoryInfo output) {
+		Console.WriteLine($"Extracting file \"{input.Name}\" into directory \"{output.FullName}\"...");
+		using var zipArchive = ZipFile.OpenRead(input.FullName);
+		zipArchive.ExtractToDirectory(output.FullName, overwriteFiles: true);
+	}
+
+	/// <summary>
+	/// Runs the specified executable with the `--version` argument.
+	/// </summary>
+	/// <param name="_">The current command.</param>
+	/// <param name="output">The path to the output directory.</param>
+	/// <param name="executable">The executable path, relative to the output directory.</param>
+	/// <returns>The standard output of the underlying process.</returns>
+	/// <exception cref="Exception">An error occurred when starting the underlying process.</exception>
+	public static string GetExecutableVersion(this Command _, DirectoryInfo output, string executable) {
+		var startInfo = new ProcessStartInfo {
+			Arguments = "--version",
+			CreateNoWindow = true,
+			FileName = Path.Join(output.FullName, executable),
+			RedirectStandardOutput = true
+		};
+
+		using var process = Process.Start(startInfo) ?? throw new Exception($"The \"{executable}\" process could not be started.");
+		var standardOutput = process.StandardOutput.ReadToEnd().Trim();
+		process.WaitForExit();
+		if (process.ExitCode != 0) throw new Exception($"The \"{executable}\" process failed with exit code {process.ExitCode}.");
+		return standardOutput;
+	}
+}
--- src/command_line/jdk_command.cs
+++ src/command_line/jdk_command.cs
@@ -1,10 +1,5 @@
 namespace Belin.Cli.CommandLine;
 
-using Belin.Diagnostics;
-using Belin.Net.Http;
-using System.Diagnostics;
-using System.IO.Compression;
-
 /// <summary>
 /// Downloads and installs the latest OpenJDK release.
 /// </summary>
@@ -33,13 +28,9 @@ public class JdkCommand: Command {
 			return 1;
 		}
 
-		using var httpClient = HttpClientFactory.CreateClient();
-		var path = await DownloadArchive(httpClient, java);
-		ExtractArchive(path, output);
-		// TODO Strip the subfolder!!!
-
-		using var process = new Process() { StartInfo = { FileName = Path.Join(output.FullName, @"bin\java.exe") } };
-		Console.WriteLine(process.GetVersion());
+		using var httpClient = this.CreateHttpClient();
+		this.ExtractZipFile(await DownloadArchive(httpClient, java), output); // TODO Strip the subfolder!!!
+		Console.WriteLine(this.GetExecutableVersion(output, @"bin\java.exe"));
 		return 0;
 	}
 
@@ -49,25 +40,13 @@ public class JdkCommand: Command {
 	/// <param name="httpClient">The HTTP client.</param>
 	/// <param name="java">The major version of the Java development kit.</param>
 	/// <returns>The path to the downloaded ZIP archive.</returns>
-	private static async Task<string> DownloadArchive(HttpClient httpClient, int java) {
+	private static async Task<FileInfo> DownloadArchive(HttpClient httpClient, int java) {
 		var path = Path.Join(Path.GetTempPath(), $"microsoft-jdk-{java}-windows-x64.zip");
 		var file = Path.GetFileName(path);
 
 		Console.WriteLine($"Downloading file \"{file}\"...");
 		var bytes = await httpClient.GetByteArrayAsync($"https://aka.ms/download-jdk/{file}");
 		File.WriteAllBytes(path, bytes);
-		return path;
-	}
-
-	/// <summary>
-	/// Extracts the ZIP archive located at the specified path.
-	/// </summary>
-	/// <param name="path">The path to the downloaded ZIP archive.</param>
-	/// <param name="output">The path to the output directory.</param>
-	private static void ExtractArchive(string path, DirectoryInfo output) {
-		// TODO Extract this method in an extension class!
-		Console.WriteLine($"Extracting file \"{Path.GetFileName(path)}\" into directory \"{output.FullName}\"...");
-		using var zipArchive = ZipFile.OpenRead(path);
-		zipArchive.ExtractToDirectory(output.FullName, overwriteFiles: true);
+		return new FileInfo(path);
 	}
 }
