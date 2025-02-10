4fdab60d200ddc861030bc2f76c74a7abcf085a8
Cédric Belin <cedric@belin.io>
2025-02-10 09:04:40 +0100

    Start the port of the `charset` and `engine` MySQL commands

--- src/command_line/mysql/engine_command.cs
+++ src/command_line/mysql/engine_command.cs
@@ -1,5 +1,8 @@
 namespace Belin.Cli.CommandLine.MySql;
 
+using Belin.Cli.Data;
+using MySqlConnector;
+
 /// <summary>
 /// Alters the storage engine of MariaDB/MySQL tables.
 /// </summary>
@@ -22,12 +25,29 @@ public class EngineCommand: Command {
 	/// </summary>
 	/// <param name="dsn">The connection string.</param>
 	/// <returns>The exit code.</returns>
-	public async Task<int> Execute(Uri dsn, string? schema, string[] tables) {
-		if (tables.Length > 0 && string.IsNullOrWhiteSpace(schema)) {
-			Console.WriteLine($"The table \"{tables[0]}\" requires that a schema be specified.");
+	public async Task<int> Execute(Uri dsn, string? schemaName, string[] tableNames) {
+		var noSchema = string.IsNullOrWhiteSpace(schemaName);
+		if (tableNames.Length > 0 && noSchema) {
+			Console.WriteLine($"The table \"{tableNames[0]}\" requires that a schema be specified.");
 			return 1;
 		}
 
-		return await Task.FromResult(0);
+		using var connection = await this.CreateDbConnection(dsn);
+		var schemas = noSchema ? connection.GetSchemas() : [new Schema { Name = schemaName! }];
+		var tables = schemas.SelectMany(schema => tableNames.Length > 0
+			? tableNames.Select(table => new Table { Name = table, Schema = schema.Name })
+			: connection.GetTables(schema));
+
+		// foreach (var table in tables) await OptimizeTable(connection, table);
+		return 0;
+	}
+
+	/// <summary>
+	/// Alters the specified database table.
+	/// </summary>
+	/// <param name="connection"></param>
+	/// <param name="table"></param>
+	/// <returns></returns>
+	private static async Task AlterTable(MySqlConnection connection, Table table) {
 	}
 }
