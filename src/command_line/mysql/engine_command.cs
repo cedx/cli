2633c709f157a9bd389dca5247de5ab42c040bc0
Cédric Belin <cedric@belin.io>
2025-02-10 10:37:08 +0100

    Finalize the `mysql engine` command

--- src/command_line/mysql/engine_command.cs
+++ src/command_line/mysql/engine_command.cs
@@ -12,20 +12,25 @@ public class EngineCommand: Command {
 	/// Creates a new command.
 	/// </summary>
 	public EngineCommand(DsnOption dsnOption): base("engine", "Alter the storage engine of MariaDB/MySQL tables.") {
+		var engineParameter = new Argument<string>("engine", "The name of the new storage engine.");
 		var schemaOption = new SchemaOption();
 		var tableOption = new TableOption();
 
+		Add(engineParameter);
 		Add(schemaOption);
 		Add(tableOption);
-		this.SetHandler(Execute, dsnOption, schemaOption, tableOption);
+		this.SetHandler(Execute, dsnOption, engineParameter, schemaOption, tableOption);
 	}
 
 	/// <summary>
 	/// Executes this command.
 	/// </summary>
 	/// <param name="dsn">The connection string.</param>
+	/// <param name="engine">The name of the new storage engine.</param>
+	/// <param name="schemaName">The schema name.</param>
+	/// <param name="tableNames">The table names.</param>
 	/// <returns>The exit code.</returns>
-	public async Task<int> Execute(Uri dsn, string? schemaName, string[] tableNames) {
+	public async Task<int> Execute(Uri dsn, string engine, string? schemaName, string[] tableNames) {
 		var noSchema = string.IsNullOrWhiteSpace(schemaName);
 		if (tableNames.Length > 0 && noSchema) {
 			Console.WriteLine($"The table \"{tableNames[0]}\" requires that a schema be specified.");
@@ -38,16 +43,27 @@ public class EngineCommand: Command {
 			? tableNames.Select(table => new Table { Name = table, Schema = schema.Name })
 			: connection.GetTables(schema));
 
-		// foreach (var table in tables) await OptimizeTable(connection, table);
+		using var disableForeignKeys = new MySqlCommand("SET foreign_key_checks = 0", connection);
+		await disableForeignKeys.ExecuteNonQueryAsync();
+		foreach (var table in tables.Where(item => string.Equals(item.Collation, engine, StringComparison.InvariantCultureIgnoreCase)))
+			await AlterTable(connection, table, engine);
+
+		using var enableForeignKeys = new MySqlCommand("SET foreign_key_checks = 1", connection);
+		await enableForeignKeys.ExecuteNonQueryAsync();
 		return 0;
 	}
 
 	/// <summary>
 	/// Alters the specified database table.
 	/// </summary>
-	/// <param name="connection"></param>
-	/// <param name="table"></param>
-	/// <returns></returns>
-	private static async Task AlterTable(MySqlConnection connection, Table table) {
+	/// <param name="connection">The database connection.</param>
+	/// <param name="table">The table to alter.</param>
+	/// <param name="engine">The name of the new storage engine.</param>
+	/// <returns>Completes when the table has been altered.</returns>
+	private static async Task AlterTable(MySqlConnection connection, Table table, string engine) {
+		var qualifiedName = table.GetQualifiedName(escape: true);
+		Console.WriteLine($"Processing: {qualifiedName}");
+		using var command = new MySqlCommand($"ALTER TABLE {qualifiedName} ENGINE = {engine}", connection);
+		await command.ExecuteNonQueryAsync();
 	}
 }
