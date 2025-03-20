fa01b0cfe3644002ec63550eed32b46a510f940f
Cédric Belin <cedric.belin@mc2it.com>
2025-03-20 11:38:17 +0100

    Add the `ApplicationConfiguration.RootPath` property

--- src/nssm/application_configuration.cs
+++ src/nssm/application_configuration.cs
@@ -4,37 +4,44 @@ using System.Text.Json;
 using System.Text.Json.Serialization;
 
 /// <summary>
-/// Provides the configuration of a Node.js application.
+/// Provides the configuration of an application.
 /// </summary>
 public class ApplicationConfiguration {
 
 	/// <summary>
 	/// The application description.
 	/// </summary>
-	[JsonPropertyName("description")]
 	public required string Description { get; set; } = string.Empty;
 
 	/// <summary>
 	/// The application identifier.
 	/// </summary>
-	[JsonPropertyName("id")]
 	public required string Id { get; init; }
 
 	/// <summary>
 	/// The application name.
 	/// </summary>
-	[JsonPropertyName("name")]
 	public required string Name { get; init; }
 
 	/// <summary>
-	/// Reads the configuration file of the Node.js application located in the specified directory.
+	/// The path to the application root directory.
 	/// </summary>
-	/// <param name="input">The path to the root directory of the Node.js application.</param>
-	/// <returns>The configuration of the specified Node.js application, or <see langword="null"/> if not found.</returns>
-	public static ApplicationConfiguration? ReadFromDirectory(DirectoryInfo input) {
+	[JsonIgnore]
+	public required string RootPath { get; set; } = string.Empty;
+
+	/// <summary>
+	/// Reads the configuration file of the application located in the specified directory.
+	/// </summary>
+	/// <param name="input">The path to the root directory of the application.</param>
+	/// <returns>The configuration of the specified application, or <see langword="null"/> if not found.</returns>
+	public static ApplicationConfiguration? ReadFromDirectory(string input) {
 		foreach (var folder in new[] { "lib/server", "lib", "src/server", "src" }) {
-			var path = Path.Join(input.FullName, folder, "appsettings.json");
-			if (File.Exists(path)) return JsonSerializer.Deserialize<ApplicationConfiguration>(File.ReadAllText(path));
+			var path = Path.Join(input, folder, "appsettings.json");
+			if (File.Exists(path)) {
+				var configuration = JsonSerializer.Deserialize<ApplicationConfiguration>(File.ReadAllText(path), JsonSerializerOptions.Web);
+				if (configuration is not null) configuration.RootPath = input;
+				return configuration;
+			}
 		}
 
 		return null;
