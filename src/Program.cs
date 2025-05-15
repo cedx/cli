29dbf9ae8b5f6a088012291f5577d9661569db1f
Cédric Belin <cedric@belin.io>
2025-05-15 19:37:24 +0200

    Port the `iconv` command

--- src/Program.cs
+++ src/Program.cs
@@ -1,13 +1,23 @@
 using Belin.Cli;
+using Microsoft.Extensions.Hosting;
+using System.CommandLine.Builder;
+using System.CommandLine.Hosting;
+using System.CommandLine.Parsing;
 
 // Configure the command line.
 var rootCommand = new RootCommand("Command line interface of Cédric Belin, full stack developer.") {
 	new IconvCommand(),
-	new MySqlCommand(),
-	new NssmCommand(),
-	new SetupCommand()
+	//new MySqlCommand(),
+	//new NssmCommand(),
+	//new SetupCommand()
 };
+
+var builder = new CommandLineBuilder(rootCommand)
+	.UseDefaults()
+	.UseHost(_ => Host.CreateDefaultBuilder(args).UseContentRoot(AppContext.BaseDirectory), builder => builder
+		// TODO ??? .ConfigureServices(Container.AddServices)
+		.UseCommandHandler<IconvCommand, IconvCommand.CommandHandler>());
 
 // Start the application.
 if (args.Length == 0) args = ["--help"];
-return await rootCommand.InvokeAsync(args);
+return await builder.Build().InvokeAsync(args);
