89d652cb2edce9e4f52d064c90d5cd3c233ebf55
Cédric Belin <cedric@belin.io>
2025-05-16 22:56:05 +0200

    Port the `nssm` commands

--- src/Nssm/RemoveCommand.cs
+++ src/Nssm/RemoveCommand.cs
@@ -1,5 +1,7 @@
 namespace Belin.Cli.Nssm;
 
+using Microsoft.Extensions.Logging;
+using System.CommandLine.Invocation;
 using System.Diagnostics;
 using System.ServiceProcess;
 
@@ -14,39 +16,58 @@ public class RemoveCommand: Command {
 	public RemoveCommand(): base("remove", "Unregister the Windows service.") =>
 		Add(new Argument<DirectoryInfo>(
 			name: "directory",
-			description: "The path to the root directory of the Node.js application.",
+			description: "The path to the root directory of the .NET or Node.js application.",
 			getDefaultValue: () => new DirectoryInfo(Environment.CurrentDirectory)
 		));
 
 	/// <summary>
-	/// Invokes this command.
+	/// The command handler.
 	/// </summary>
-	/// <param name="directory">The path to the root directory of the Node.js application.</param>
-	/// <returns>The exit code.</returns>
-	public Task<int> Invoke(DirectoryInfo directory) {
-		if (!this.CheckPrivilege()) return Task.FromResult(1);
+	/// <param name="logger">The logging service.</aparam>
+	public class CommandHandler(ILogger<InstallCommand> logger): ICommandHandler {
 
-		var application = WebApplication.ReadFromDirectory(directory.FullName);
-		if (application is null) {
-			Console.WriteLine("Unable to locate the application configuration file.");
-			return Task.FromResult(2);
+		/// <summary>
+		/// The path to the root directory of the .NET or Node.js application.
+		/// </summary>
+		public required DirectoryInfo Directory { get; set; }
+
+		/// <summary>
+		/// Invokes this command.
+		/// </summary>
+		/// <param name="context">The invocation context.</param>
+		/// <returns>The exit code.</returns>
+		public int Invoke(InvocationContext context) {
+			if (!this.CheckPrivilege()) return 1;
+
+			var application = WebApplication.ReadFromDirectory(Directory.FullName);
+			if (application is null) {
+				logger.LogError("Unable to locate the application configuration file.");
+				return 2;
+			}
+
+			using var serviceController = new ServiceController(application.Id);
+			if (serviceController.Status == ServiceControllerStatus.Running) {
+				serviceController.Stop();
+				serviceController.WaitForStatus(ServiceControllerStatus.Stopped);
+			}
+
+			try {
+				using var process = Process.Start("nssm", ["remove", application.Id, "confirm"]) ?? throw new ProcessException("nssm");
+				process.WaitForExit();
+				if (process.ExitCode != 0) throw new ProcessException("nssm", $"The process failed with exit code {process.ExitCode}.");
+				return 0;
+			}
+			catch (Exception e) {
+				logger.LogError("{Message}", e.Message);
+				return 3;
+			}
 		}
 
-		using var serviceController = new ServiceController(application.Id);
-		if (serviceController.Status == ServiceControllerStatus.Running) {
-			serviceController.Stop();
-			serviceController.WaitForStatus(ServiceControllerStatus.Stopped);
-		}
-
-		try {
-			using var process = Process.Start("nssm", ["remove", application.Id, "confirm"]) ?? throw new ProcessException("nssm");
-			process.WaitForExit();
-			if (process.ExitCode != 0) throw new ProcessException("nssm", $"The process failed with exit code {process.ExitCode}.");
-			return Task.FromResult(0);
-		}
-		catch (Exception e) {
-			Console.WriteLine(e.Message);
-			return Task.FromResult(3);
-		}
+		/// <summary>
+		/// Invokes this command.
+		/// </summary>
+		/// <param name="context">The invocation context.</param>
+		/// <returns>The exit code.</returns>
+		public Task<int> InvokeAsync(InvocationContext context) => Task.FromResult(Invoke(context));
 	}
 }
