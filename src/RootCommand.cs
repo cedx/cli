a0a3b4cbdb6ebf216eb9dff5073bb55630febf7d
Cédric Belin <cedric@belin.io>
2025-07-12 22:58:18 +0200

    Code formatting

--- src/RootCommand.cs
+++ src/RootCommand.cs
@@ -12,7 +12,8 @@ public class RootCommand: System.CommandLine.RootCommand {
 	/// <param name="mysql">The <c>mysql</c> subcommand.</param>
 	/// <param name="nssm">The <c>nssm</c> subcommand.</param>
 	/// <param name="setup">The <c>setup</c> subcommand.</param>
-	public RootCommand(IconvCommand iconv, MySqlCommand mysql, NssmCommand nssm, SetupCommand setup): base("Command line interface of Cédric Belin, full stack developer.") {
+	public RootCommand(IconvCommand iconv, MySqlCommand mysql, NssmCommand nssm, SetupCommand setup):
+	base("Command line interface of Cédric Belin, full stack developer.") {
 		Subcommands.Add(iconv);
 		Subcommands.Add(mysql);
 		Subcommands.Add(nssm);
