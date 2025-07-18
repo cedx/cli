2b64a2fd0a7cd5c284d0f3553bd71a49238e604b
Cédric Belin <cedric.belin@mc2it.com>
2025-07-18 12:28:23 +0200

    Update the documentation

--- src/CommandExtensions.cs
+++ src/CommandExtensions.cs
@@ -8,10 +8,10 @@ public static class CommandExtensions {
 	/// <summary>
 	/// Checks whether this command should be executed in an elevated prompt.
 	/// </summary>
-	/// <param name="command">The current command.</param>
+	/// <param name="_">The current command.</param>
 	/// <param name="output">The path to the output directory.</param>
 	/// <returns><see langword="true"/> if this command should be executed in an elevated prompt, otherwise <see langword="false"/>.</returns>
-	public static bool CheckPrivilege(this Command command, DirectoryInfo? output = null) {
+	public static bool CheckPrivilege(this Command _, DirectoryInfo? output = null) {
 		var isPrivileged = Environment.IsPrivilegedProcess;
 		if (output is not null) {
 			var home = new DirectoryInfo(Environment.GetFolderPath(Environment.SpecialFolder.Personal));
