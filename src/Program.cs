6f645a578ce7ba90d9f5cb0f92f63637686a661c
Cédric Belin <cedx@outlook.com>
2025-07-28 22:15:35 +0200

    Set the process title

--- src/Program.cs
+++ src/Program.cs
@@ -1,5 +1,10 @@
 using Belin.Cli;
 using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+// Get information about the program.
+var assembly = typeof(Program).Assembly;
+var product = assembly.GetCustomAttribute<AssemblyProductAttribute>()!.Product;
 
 // Configure the dependency container.
 var services = new ServiceCollection();
@@ -7,6 +12,8 @@ services.AddServices();
 services.AddCommands();
 
 // Start the application.
+Console.Title = product;
+Thread.Sleep(3000);
 using var serviceProvider = services.BuildServiceProvider();
 var rootCommand = serviceProvider.GetRequiredService<Belin.Cli.RootCommand>();
 return await rootCommand.Parse(args.Length > 0 ? args : ["--help"]).InvokeAsync();
