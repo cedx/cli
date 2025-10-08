21f67aec75a987ac705e3e6eaca0570eac4964c5
Cédric Belin <cedx@outlook.com>
2025-10-08 23:12:36 +0200

    Fix the build system

--- tool/Publish.ps1
+++ tool/Publish.ps1
@@ -1,6 +1,6 @@
 . $PSScriptRoot/Default.ps1
 
 Write-Host "Publishing the package..."
-$version = Select-Xml "//Version" Package.xml
+$version = (Select-Xml "//Version" Package.xml).Node.InnerText
 git tag "v$version"
 git push origin "v$version"
