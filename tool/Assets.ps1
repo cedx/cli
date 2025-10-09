7c4848f22b7d11db660a31229623f1ac9c7ab480
Cédric Belin <cedx@outlook.com>
2025-10-09 11:15:20 +0200

    Code optimization

--- tool/Assets.ps1
+++ tool/Assets.ps1
@@ -1,6 +1,6 @@
 Write-Output "Deploying the assets..."
-foreach ($item in "Binary", "Text") {
-	$file = "$($item.ToLowerInvariant())-extensions";
+"Binary", "Text" | ForEach-Object {
+	$file = "$($_.ToLowerInvariant())-extensions";
 	$path = "sindresorhus/$file/refs/heads/main/$file.json";
 	Invoke-WebRequest "https://raw.githubusercontent.com/$path" -OutFile "res/${item}Extensions.json"
 }
