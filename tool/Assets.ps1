154637a88eeeb17ec5e8badacde3d37ba25dafa5
Cédric Belin <cedric.belin@mc2it.com>
2025-10-10 10:34:32 +0200

    Fix the "Assets" script

--- tool/Assets.ps1
+++ tool/Assets.ps1
@@ -2,5 +2,5 @@ Write-Output "Deploying the assets..."
 "Binary", "Text" | ForEach-Object {
 	$file = "$($_.ToLowerInvariant())-extensions";
 	$path = "sindresorhus/$file/refs/heads/main/$file.json";
-	Invoke-WebRequest "https://raw.githubusercontent.com/$path" -OutFile "res/${item}Extensions.json"
+	Invoke-WebRequest "https://raw.githubusercontent.com/$path" -OutFile "res/${_}Extensions.json"
 }
