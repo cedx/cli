6c9139194afcab79be123eafc0c6fad35d823f15
Cédric Belin <cedx@outlook.com>
2025-10-10 19:44:24 +0200

    Code formatting

--- tool/Assets.ps1
+++ tool/Assets.ps1
@@ -1,6 +1,6 @@
 Write-Output "Deploying the assets..."
-"Binary", "Text" | ForEach-Object {
-	$file = "$($_.ToLowerInvariant())-extensions";
+foreach ($item in "Binary", "Text") {
+	$file = "$($item.ToLowerInvariant())-extensions";
 	$path = "sindresorhus/$file/refs/heads/main/$file.json";
-	Invoke-WebRequest "https://raw.githubusercontent.com/$path" -OutFile "res/${_}Extensions.json"
+	Invoke-WebRequest "https://raw.githubusercontent.com/$path" -OutFile "res/${item}Extensions.json"
 }
--- tool/Version.ps1
+++ tool/Version.ps1
@@ -2,6 +2,6 @@ Write-Output "Updating the version number in the sources..."
 $version = (Select-Xml "//Version" Package.xml).Node.InnerText
 (Get-Content "ReadMe.md") -replace "project/v\d+(\.\d+){2}", "project/v$version" | Out-File "ReadMe.md"
 (Get-Content "Setup.iss") -replace 'version "\d+(\.\d+){2}"', "version ""$version""" | Out-File "Setup.iss"
-Get-ChildItem "*/*.csproj" | ForEach-Object {
-	(Get-Content $_) -replace "<Version>\d+(\.\d+){2}</Version>", "<Version>$version</Version>" | Out-File $_
+foreach ($item in Get-ChildItem "*/*.csproj") {
+	(Get-Content $item) -replace "<Version>\d+(\.\d+){2}</Version>", "<Version>$version</Version>" | Out-File $item
 }
