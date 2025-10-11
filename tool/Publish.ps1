615c6deac3e57973f735968f24aefb285a98a291
Cédric Belin <cedx@outlook.com>
2025-10-11 21:47:42 +0200

    Replace the "Package.xml" file by a PowerShell data file

--- tool/Publish.ps1
+++ tool/Publish.ps1
@@ -1,6 +1,6 @@
 . $PSScriptRoot/Default.ps1
 
 Write-Output "Publishing the package..."
-$version = (Select-Xml "//Version" Package.xml).Node.InnerText
+$version = (Import-PowerShellDataFile "Package.psd1").Version
 git tag "v$version"
 git push origin "v$version"
--- tool/Version.ps1
+++ tool/Version.ps1
@@ -1,5 +1,5 @@
 Write-Output "Updating the version number in the sources..."
-$version = (Select-Xml "//Version" Package.xml).Node.InnerText
+$version = (Import-PowerShellDataFile "Package.psd1").Version
 (Get-Content "ReadMe.md") -replace "project/v\d+(\.\d+){2}", "project/v$version" | Out-File "ReadMe.md"
 (Get-Content "Setup.iss") -replace 'version "\d+(\.\d+){2}"', "version ""$version""" | Out-File "Setup.iss"
 foreach ($item in Get-ChildItem "*/*.csproj") {
