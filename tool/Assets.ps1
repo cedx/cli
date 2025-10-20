f8eb1c6a4cc65d2ffcb52a236be7219f975c1087
Cédric Belin <cedx@outlook.com>
2025-10-21 00:22:04 +0200

    Update the "Assets" task

--- tool/Assets.ps1
+++ tool/Assets.ps1
@@ -1,6 +1,6 @@
 "Deploying the assets..."
 foreach ($item in "Binary", "Text") {
 	$file = "$($item.ToLowerInvariant())-extensions";
-	$path = "sindresorhus/$file/refs/heads/main/$file.json";
+	$path = "sindresorhus/$file/main/$file.json";
 	Invoke-WebRequest "https://raw.githubusercontent.com/$path" -OutFile "res/${item}Extensions.json"
 }
