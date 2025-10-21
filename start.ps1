96fde79e5bc37a3e34dbdc67472e71ba958f7a2c
Cédric Belin <cedx@outlook.com>
2025-10-21 19:52:32 +0200

    Update the launch script

--- start.ps1
+++ start.ps1
@@ -1,8 +1,17 @@
 #!/usr/bin/env pwsh
+param (
+	[Parameter(Mandatory, Position = 0)]
+	[ValidateSet("Iconv", "MySql", "Nssm", "Setup")]
+	[string] $Command
+)
+
 Set-StrictMode -Version Latest
 $ErrorActionPreference = "Stop"
 $PSNativeCommandUseErrorActionPreference = $true
 
 $commandPath = Get-Item $PSCommandPath
 $scriptRoot = $commandPath.LinkType ? (Split-Path $commandPath.LinkTarget) : $PSScriptRoot
-dotnet $scriptRoot/bin/Belin.Cli.dll @args
+
+$script = "$scriptRoot/src/$Command.ps1"
+if (Test-Path $script) { . $script @args }
+else { Write-Error "TODO" }
