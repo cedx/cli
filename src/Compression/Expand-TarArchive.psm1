35edce021c746373432dfdb96af5f05d2b000bcd
Cédric Belin <cedx@outlook.com>
2025-10-26 14:40:09 +0100

    Code optimization

---
+++ src/Compression/Expand-TarArchive.psm1
@@ -0,0 +1,21 @@
+<#
+.SYNOPSIS
+	Extracts the specified TAR archive into a given directory.
+.PARAMETER Path
+	The path to the input TAR archive.
+.PARAMETER DestinationPath
+	The path to the output directory.
+.PARAMETER Skip
+	The number of leading directory components to remove from file names on extraction.
+#>
+function Expand-TarArchive {
+	[OutputType([void])]
+	param (
+		[Parameter(Mandatory, Position = 0)] [ValidateScript({ Test-Path $_ -IsValid })] [string] $Path,
+		[Parameter(Mandatory, Position = 1)] [ValidateScript({ Test-Path $_ -IsValid })] [string] $DestinationPath,
+		[ValidateRange("NonNegative")] [int] $Skip = 0
+	)
+
+	New-Item $DestinationPath -Force -ItemType Directory | Out-Null
+	tar --directory $DestinationPath --extract --file $Path --strip-components $Skip
+}
--- src/Install-Node.psm1
+++ src/Install-Node.psm1
@@ -1,5 +1,5 @@
-using namespace System.IO
-using module ./Compression/Expand-Archive.psm1
+using module ./Compression/Expand-TarArchive.psm1
+using module ./Compression/Expand-ZipArchive.psm1
 
 <#
 .SYNOPSIS
@@ -13,30 +13,32 @@ function Install-Node {
 	param (
 		[Parameter(Position = 0)]
 		[ValidateScript({ Test-Path $_ -IsValid })]
-		[string] $Path = $IsWindows ? "C:\Program Files\Node.js" : "/usr/local"
+		[string] $Path = $IsWindows ? "C:\Program Files\Node.js" : "/usr/local",
+
+		# TODO ?
+		[ValidateScript({ Test-Path $_ -IsValid })]
+		[string] $ConfigFile
 	)
 
-	end {
-		$platform, $extension = switch ($true) {
-			($IsMacOS) { "darwin", "tar.gz"; break }
-			($IsWindows) { "win", "zip"; break }
-			default { "linux", "tar.xz" }
-		}
-
-		"Fetching the list of Node.js releases..."
-		$response = Invoke-RestMethod "https://nodejs.org/dist/index.json"
-		$version = [version] $response[0].version.Substring(1)
-
-		$file = "node-v$version-$platform-x64.$extension"
-		"Downloading file ""$file""..."
-		$outputFile = New-TemporaryFile
-		Invoke-WebRequest "https://nodejs.org/dist/v$version/$file" -OutFile $outputFile
-
-		"Extracting file ""$file"" into directory ""$Path""..."
-		if ($extension -eq "zip") { Expand-ZipArchive $outputFile $Path -Skip 1 }
-		else { Expand-TarArchive $outputFile $Path -Skip 1 }
-
-		$executable = $IsWindows ? "node.exe" : "bin/node"
-		& $Path/$executable --version
+	$platform, $extension = switch ($true) {
+		($IsMacOS) { "darwin", "tar.gz"; break }
+		($IsWindows) { "win", "zip"; break }
+		default { "linux", "tar.xz" }
 	}
+
+	"Fetching the list of Node.js releases..."
+	$response = Invoke-RestMethod "https://nodejs.org/dist/index.json"
+	$version = [version] $response[0].version.Substring(1)
+
+	$file = "node-v$version-$platform-x64.$extension"
+	"Downloading file ""$file""..."
+	$outputFile = New-TemporaryFile
+	Invoke-WebRequest "https://nodejs.org/dist/v$version/$file" -OutFile $outputFile
+
+	"Extracting file ""$file"" into directory ""$Path""..."
+	if ($extension -eq "zip") { Expand-ZipArchive $outputFile $Path -Skip 1 }
+	else { Expand-TarArchive $outputFile $Path -Skip 1 }
+
+	$executable = $IsWindows ? "node.exe" : "bin/node"
+	& $Path/$executable --version
 }
