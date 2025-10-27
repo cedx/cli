4886141cb77c19c22976c51da6563da6b4bb02f5
Cédric Belin <cedx@outlook.com>
2025-10-27 12:06:50 +0100

    Code formatting

--- start.ps1
+++ start.ps1
@@ -1,12 +1,19 @@
 #!/usr/bin/env pwsh
 param (
-	[Parameter(Mandatory, Position = 0)] [string] $Command,
-	[Parameter(Position = 1, ValueFromRemainingArguments)] [string[]] $Arguments
+	[Parameter(Mandatory, Position = 0)]
+	[ArgumentCompleter({
+		param ($commandName, $parameterName, $wordToComplete)
+		(Get-Item "$PSScriptRoot/src/$wordToComplete*.psm1").BaseName
+	})]
+	[string] $Command,
+
+	[Parameter(Position = 1, ValueFromRemainingArguments)]
+	[string[]] $Arguments
 )
 
-Set-StrictMode -Version Latest
 $ErrorActionPreference = "Stop"
 $PSNativeCommandUseErrorActionPreference = $true
+Set-StrictMode -Version Latest
 
 $commandPath = Get-Item $PSCommandPath
 $scriptRoot = $commandPath.LinkType ? (Split-Path $commandPath.LinkTarget) : $PSScriptRoot
