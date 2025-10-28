57d19bfd3253d0c83ddf1386cb2495181587cae0
Cédric Belin <cedx@outlook.com>
2025-10-28 12:06:34 +0100

    Add the `Get-Engines` function

---
+++ src/MySql/Get-Engines.psm1
@@ -0,0 +1,22 @@
+using namespace System.Collections.Generic
+
+<#
+.SYNOPSIS
+	Gets the list of all storage engines.
+.PARAMETER Connection
+	The database connection.
+.OUTPUTS
+	The list of all storage engines.
+#>
+function Get-Engines {
+	[OutputType([string[]])]
+	param ([Parameter(Mandatory, Position = 0)] [ValidateNotNull()] [MySqlConnection] $Connection)
+
+	$command = [MySqlCommand]::new("SHOW ENGINES", $Connection)
+	$reader = $command.ExecuteReader()
+
+	$list = [List[string]]::new()
+	while ($reader.Read()) { $list.Add($reader["Engine"]) }
+	$reader.Close()
+	$list.ToArray()
+}
