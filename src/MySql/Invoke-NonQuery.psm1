f9ad11941f9fbb827823588659a0cd0acd74cede
Cédric Belin <cedx@outlook.com>
2025-10-28 12:06:34 +0100

    Add the `Get-Engines` function

--- src/MySql/Invoke-NonQuery.psm1
+++ src/MySql/Invoke-NonQuery.psm1
@@ -7,16 +7,24 @@ function Invoke-NonQuery {
 	[OutputType([Result])]
 	param ([Parameter(Mandatory, Position = 0)] [ValidateNotNull()] [MySqlCommand] $Command)
 
-	$result = [Result]::Success()
-	$reader = $Command.ExecuteReader()
-	while ($reader.Read()) {
-		switch ($reader["Msg_type"]) {
-			"error" { $result.Message = $reader["Msg_text"]; break }
-			"note" { $result.Message = $reader["Msg_text"]; break }
-			"status" { $result.IsFailure = -not ($result.IsSuccess = $reader["Msg_text"] -eq "OK"); break }
+	$reader = $null
+	try {
+		$result = [Result]::Success()
+		$reader = $Command.ExecuteReader()
+		while ($reader.Read()) {
+			switch ($reader["Msg_type"]) {
+				"error" { $result.Message = $reader["Msg_text"]; break }
+				"note" { $result.Message = $reader["Msg_text"]; break }
+				"status" { $result.IsFailure = -not ($result.IsSuccess = $reader["Msg_text"] -eq "OK"); break }
+			}
 		}
-	}
 
-	$reader.Close()
-	$result
+		$result
+	}
+	catch {
+		[Result]::Failure($_.ErrorDetails?.Message ?? $_.Exception.Message)
+	}
+	finally {
+		${reader}?.Close()
+	}
 }
