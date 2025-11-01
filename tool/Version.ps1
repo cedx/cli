ad2a1fc2d1ac5ce07cb2c13374fc2a1ebf219c90
Cédric Belin <cedx@outlook.com>
2025-11-01 20:52:47 +0100

    Update the changelog

--- tool/Version.ps1
+++ tool/Version.ps1
@@ -1,4 +1,3 @@
 "Updating the version number in the sources..."
 $version = (Import-PowerShellDataFile "Cli.psd1").ModuleVersion
 (Get-Content "ReadMe.md") -replace "module/v\d+(\.\d+){2}", "module/v$version" | Out-File "ReadMe.md"
-(Get-Content "Setup.iss") -replace 'version "\d+(\.\d+){2}"', "version ""$version""" | Out-File "Setup.iss"
