0f24c05e8937f45446f9e5482014b327f2f43b18
Cédric Belin <cedx@outlook.com>
2025-11-01 22:28:05 +0100

    Update the "Publish" script

--- tool/Publish.ps1
+++ tool/Publish.ps1
@@ -6,12 +6,12 @@ $version = (Import-PowerShellDataFile $module).ModuleVersion
 git tag "v$version"
 git push origin "v$version"
 
-$output = "var/Package"
-New-Item $output -ItemType Directory
+$output = "var/$($module.BaseName)"
+New-Item $output -ItemType Directory | Out-Null
 Copy-Item $module $output
 Copy-Item *.md $output
 Copy-Item bin $output -Recurse
 Copy-Item src $output -Recurse
 
 Compress-PSResource $output var
-Publish-PSResource -ApiKey $Env:PSGALLERY_API_KEY -NupkgPath "var/$($module.BaseName).$version.nupkg" -Verbose -WhatIf
+Publish-PSResource -ApiKey $Env:PSGALLERY_API_KEY -NupkgPath "var/$($module.BaseName).$version.nupkg"
