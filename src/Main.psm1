39461c38e293150701ea65e8b601f41d039f334c
Cédric Belin <cedx@outlook.com>
2025-11-08 18:54:41 +0100

    The `Get-CliVersion` function now returns a `[semver]` instance

--- src/Main.psm1
+++ src/Main.psm1
@@ -5,6 +5,9 @@
 	The version number of the application.
 #>
 function Get-CliVersion {
-	[OutputType([string])] param ()
-	(Import-PowerShellDataFile "$PSScriptRoot/../Cli.psd1").ModuleVersion
+	[CmdletBinding()]
+	[OutputType([semver])] param ()
+
+	$module = Import-PowerShellDataFile "$PSScriptRoot/../Cli.psd1"
+	[semver] $module.ModuleVersion
 }
