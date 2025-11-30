954628d76ed1bad700c052e72604f463aa2957c3
Cédric Belin <cedx@outlook.com>
2025-11-30 18:30:04 +0100

    Update the build system

--- tool/Clean.ps1
+++ tool/Clean.ps1
@@ -1,3 +1,4 @@
 "Deleting all generated files..."
-Remove-Item "lib" -ErrorAction Ignore -Force -Recurse
+Remove-Item "bin" -ErrorAction Ignore -Force -Recurse
+Remove-Item "*/obj" -Force -Recurse
 Remove-Item "var/*" -Exclude ".gitkeep" -Force -Recurse
