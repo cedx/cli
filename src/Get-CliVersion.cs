e5cd5ab3e44812cfb8c37da7a1647b8484d64b86
Cédric Belin <cedx@outlook.com>
2025-12-04 17:50:19 +0100

    Add the `-PassThru` parameter

--- src/Get-CliVersion.cs
+++ src/Get-CliVersion.cs
@@ -1,15 +1,27 @@
 namespace Belin.Cli;
 
+using System.Reflection;
+
 /// <summary>
 /// Returns the version number of the application.
 /// </summary>
 [Cmdlet(VerbsCommon.Get, "CliVersion")]
-[OutputType(typeof(Version))]
+[OutputType(typeof(string)), OutputType(typeof(Version))]
 public class GetCliVersion: Cmdlet {
+
+	/// <summary>
+	/// Value indicating whether to return a <see cref="Version"/> object.
+	/// </summary>
+	[Parameter]
+	public SwitchParameter PassThru { get; set; }
 
 	/// <summary>
 	/// Performs execution of this command.
 	/// </summary>
-	protected override void ProcessRecord() =>
-		WriteObject(GetType().Assembly.GetName().Version);
+	protected override void ProcessRecord() {
+		var assembly = GetType().Assembly;
+		var product = assembly.GetCustomAttribute<AssemblyProductAttribute>()!.Product;
+		var version = assembly.GetName().Version!;
+		WriteObject(PassThru ? version : $"{product} {version.ToString(3)}");
+	}
 }
