b694c6e0e85d739a5785cb9c5bbe29a9142b8a30
Cédric Belin <cedx@outlook.com>
2025-12-07 23:57:03 +0100

    Add the `ValidatePath` attribute

---
+++ src/Validation/ValidatePathAttribute.cs
@@ -0,0 +1,22 @@
+namespace Belin.Cli.Validation;
+
+using Microsoft.PowerShell.Commands;
+
+/// <summary>
+/// Validates that the specified path is valid.
+/// </summary>
+/// <param name="errorMessage">The custom error message that is displayed to the user.</param>
+internal class ValidatePathAttribute(string errorMessage): ValidateArgumentsAttribute {
+
+	/// <summary>
+	/// Verifies that the value of <c>arguments</c> is valid.
+	/// </summary>
+	/// <param name="arguments">The argument value to validate.</param>
+	/// <param name="engineIntrinsics">The engine APIs for the context under which the prerequisite is being evaluated.</param>
+	/// <exception cref="ValidationMetadataException">The validation failed.</exception>
+  protected override void Validate(object arguments, EngineIntrinsics engineIntrinsics) {
+		var paths = arguments as string[];
+		var isValid = new TestPathCommand { IsValid = true, LiteralPath = paths }.Invoke<bool>().Single();
+		if (!isValid) throw new ValidationMetadataException(errorMessage);
+  }
+}
