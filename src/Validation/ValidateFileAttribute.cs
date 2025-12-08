8dbd5927784a935f25df30c21c688842757e7a0b
Cédric Belin <cedx@outlook.com>
2025-12-08 17:59:31 +0100

    Add the `ValidateFile` attribute

---
+++ src/Validation/ValidateFileAttribute.cs
@@ -0,0 +1,22 @@
+namespace Belin.Cli.Validation;
+
+using Microsoft.PowerShell.Commands;
+
+/// <summary>
+/// Validates that the specified path is an existing file.
+/// </summary>
+/// <param name="errorMessage">The custom error message that is displayed to the user.</param>
+internal class ValidateFileAttribute(string errorMessage): ValidateArgumentsAttribute {
+
+	/// <summary>
+	/// Verifies that the value of <c>arguments</c> is valid.
+	/// </summary>
+	/// <param name="arguments">The argument value to validate.</param>
+	/// <param name="engineIntrinsics">The engine APIs for the context under which the prerequisite is being evaluated.</param>
+	/// <exception cref="ValidationMetadataException">The validation failed.</exception>
+  protected override void Validate(object arguments, EngineIntrinsics engineIntrinsics) {
+		var path = arguments as string;
+		var exists = new TestPathCommand { Path = [path], PathType = TestPathType.Leaf }.Invoke<bool>().Single();
+		if (!exists) throw new ValidationMetadataException(errorMessage);
+  }
+}
