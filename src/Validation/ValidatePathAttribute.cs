60441d241f96c9092498b2c08461056d9453fcad
Cédric Belin <cedx@outlook.com>
2025-12-09 18:28:38 +0100

    Code optimization

--- src/Validation/ValidatePathAttribute.cs
+++ src/Validation/ValidatePathAttribute.cs
@@ -1,12 +1,15 @@
 namespace Belin.Cli.Validation;
 
-using Microsoft.PowerShell.Commands;
-
 /// <summary>
 /// Validates that the specified path is valid.
 /// </summary>
 /// <param name="errorMessage">The custom error message that is displayed to the user.</param>
-internal class ValidatePathAttribute(string errorMessage): ValidateArgumentsAttribute {
+public class ValidatePathAttribute(string errorMessage): ValidateArgumentsAttribute {
+
+	/// <summary>
+	/// An array containing the characters that are not allowed in path names.
+	/// </summary>
+	private static readonly char[] invalidCharacters = Path.GetInvalidPathChars();
 
 	/// <summary>
 	/// Verifies that the value of <c>arguments</c> is valid.
@@ -15,7 +18,6 @@ internal class ValidatePathAttribute(string errorMessage): ValidateArgumentsAttr
 	/// <param name="engineIntrinsics">The engine APIs for the context under which the prerequisite is being evaluated.</param>
 	/// <exception cref="ValidationMetadataException">The validation failed.</exception>
   protected override void Validate(object arguments, EngineIntrinsics engineIntrinsics) {
-		var isValid = new TestPathCommand { IsValid = true, LiteralPath = arguments as string[] }.Invoke<bool>().Single();
-		if (!isValid) throw new ValidationMetadataException(errorMessage);
+		if (arguments is not string path || invalidCharacters.Any(path.Contains)) throw new ValidationMetadataException(errorMessage);
   }
 }
