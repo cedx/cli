18225c0734880ab91d703e8cb127528aeb3adc32
Cédric Belin <cedx@outlook.com>
2026-01-27 17:13:06 +0100

    Update the solution

--- tool/Clean.ps1
+++ tool/Clean.ps1
@@ -1,4 +1,3 @@
 "Deleting all generated files..."
-Remove-Item bin -ErrorAction Ignore -Force -Recurse
-Remove-Item */obj -Force -Recurse
+Remove-Item bin, obj -ErrorAction Ignore -Force -Recurse
 Remove-Item var/* -Exclude .gitkeep -Force -Recurse
--- tool/Version.ps1
+++ tool/Version.ps1
@@ -1,5 +1,3 @@
 "Updating the version number in the sources..."
 $version = Import-PowerShellDataFile Cli.psd1 | Select-Object -ExpandProperty ModuleVersion
-Get-Item */*.csproj | ForEach-Object {
-	(Get-Content $_) -replace "<Version>\d+(\.\d+){2}</Version>", "<Version>$version</Version>" | Out-File $_
-}
+(Get-Content Cli.csproj) -replace "<Version>\d+(\.\d+){2}</Version>", "<Version>$version</Version>" | Out-File Cli.csproj
