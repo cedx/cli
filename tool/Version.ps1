97bebaaa7d6048d5367a7cdc1d80b21405b7e2ce
Cédric Belin <cedx@outlook.com>
2026-02-05 12:28:26 +0100

    Code optimization

--- tool/Version.ps1
+++ tool/Version.ps1
@@ -1,3 +1,3 @@
 "Updating the version number in the sources..."
 $version = Import-PowerShellDataFile Cli.psd1 | Select-Object -ExpandProperty ModuleVersion
-(Get-Content Cli.csproj) -replace "<Version>\d+(\.\d+){2}</Version>", "<Version>$version</Version>" | Out-File Cli.csproj
+(Get-Content Cli.csproj -Raw) -replace "<Version>\d+(\.\d+){2}</Version>", "<Version>$version</Version>" | Set-Content Cli.csproj -NoNewline
