a12a23b0a7ebf2144e0be48e10be7ddd2696ad04
Cédric Belin <cedx@outlook.com>
2026-04-03 20:53:24 +0200

    Port the `Get-CliVersion` cmdlet

---
+++ src/Get-CliVersion.psm1
@@ -0,0 +1,19 @@
+<#
+.SYNOPSIS
+	Returns the version number of the application.
+.OUTPUTS
+	The version number of the application.
+#>
+function Get-CliVersion {
+	[CmdletBinding()]
+	[OutputType([string])]
+	[OutputType([version])]
+	param (
+		# Value indicating whether to return a `System.Version` object.
+		[switch] $PassThru
+	)
+
+	$module = "$PSScriptRoot/../Belin.Cli.psd1"
+	$version = (Import-PowerShellDataFile ((Test-Path $module) ? $module : "$PSScriptRoot/../Cli.psd1")).ModuleVersion
+	$PassThru ? [version] $version : "Cédric Belin's CLI $version"
+}
